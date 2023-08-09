33aec972acc31adf118a3799b88f458dd1af6551
kakuilan <kakuilan@163.com>
2023-08-09 14:15:04 +0800

    成员变量

---
+++ Test012_RunoobTest.java
@@ -0,0 +1,26 @@
+//成员变量
+public class Test012_RunoobTest {
+    // 成员变量
+    private int instanceVar;
+    // 静态变量
+    private static int staticVar;
+
+    public void method(int paramVar) {
+        // 局部变量
+        int localVar = 10;
+
+        // 使用变量
+        instanceVar = localVar;
+        staticVar = paramVar;
+
+        System.out.println("成员变量: " + instanceVar);
+        System.out.println("静态变量: " + staticVar);
+        System.out.println("参数变量: " + paramVar);
+        System.out.println("局部变量: " + localVar);
+    }
+
+    public static void main(String[] args) {
+        Test012_RunoobTest v = new Test012_RunoobTest();
+        v.method(20);
+    }
+}
