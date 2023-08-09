ddd1c677b5ac4e6cd22e1be51d3a7f07693c8e1c
kakuilan <kakuilan@163.com>
2023-08-09 14:12:53 +0800

    变量声明

---
+++ Test011.java
@@ -0,0 +1,13 @@
+//变量声明
+public class Test011 {
+    public static void main(String[] args) {
+        int a, b, c; // 声明三个int型整数：a、 b、c
+        int d = 3, e = 4, f = 5; // 声明三个整数并赋予初值
+        byte z = 22; // 声明并初始化 z
+        String s = "runoob";  // 声明并初始化字符串 s
+        double pi = 3.14159; // 声明了双精度浮点型变量 pi
+        char x = 'x'; // 声明变量 x 的值是字符 'x'
+
+        System.out.println(s);
+    }
+}
