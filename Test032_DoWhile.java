d10b32f616ef255ab064e305e6b843797a5601e9
kakuilan <kakuilan@163.com>
2023-08-10 15:46:00 +0800

    do…while 循环

---
+++ Test032_DoWhile.java
@@ -0,0 +1,12 @@
+//do…while 循环
+public class Test032_DoWhile {
+    public static void main(String[] args) {
+        int x = 10;
+
+        do {
+            System.out.print("value of x : " + x);
+            x++;
+            System.out.print("\n");
+        } while (x < 20);
+    }
+}
