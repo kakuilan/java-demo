d90c2ebaa0901bae5e7616035f54c535f6a3beac
kakuilan <kakuilan@163.com>
2023-08-10 14:54:55 +0800

    abstract 修饰符,抽象类

---
+++ Test021_Caravan.java
@@ -0,0 +1,10 @@
+//abstract 修饰符,抽象类
+abstract class Test021_Caravan {
+    private double price;
+    private String model;
+    private String year;
+
+    public abstract void goFast(); //抽象方法
+
+    public abstract void changeColor();
+}
---
+++ Test022_SuperClass.java
@@ -0,0 +1,14 @@
+//抽象方法和实现
+
+//父级抽象类
+public abstract class Test022_SuperClass {
+    abstract void m(); //抽象方法
+}
+
+//子类
+class Test022_SubClass extends Test022_SuperClass {
+    //实现抽象方法
+    void m() {
+        //
+    }
+}
