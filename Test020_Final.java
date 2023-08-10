22c9961d27d0ddf54cc2aa249188265c215aa4ff
kakuilan <kakuilan@163.com>
2023-08-10 14:46:29 +0800

    final 修饰符

---
+++ Test020_Final.java
@@ -0,0 +1,24 @@
+//final 修饰符
+
+//final 类
+public final class Test020_Final {
+
+    final int value = 10;
+
+    // 下面是声明常量的实例
+    public static final int BOXWIDTH = 6;
+
+    //final 变量
+    static final String TITLE = "Manager";
+
+    public void changeValue() {
+        //value = 12; //将输出一个错误
+    }
+
+    //final 方法
+    public final void changeName() {
+        // 方法体
+    }
+
+
+}
