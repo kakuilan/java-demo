d56a8c0ed9527fed3983b193340ac0ca92b129b7
kakuilan <kakuilan@163.com>
2023-08-10 15:47:20 +0800

    for循环

---
+++ Test033_For.java
@@ -0,0 +1,10 @@
+//for循环
+public class Test033_For {
+    public static void main(String[] args) {
+
+        for (int x = 10; x < 20; x = x + 1) {
+            System.out.print("value of x : " + x);
+            System.out.print("\n");
+        }
+    }
+}
