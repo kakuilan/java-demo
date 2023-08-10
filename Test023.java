361ee765fb1981b6d00aadf6c5958febb05d1c2a
kakuilan <kakuilan@163.com>
2023-08-10 15:11:31 +0800

    算术运算符

---
+++ Test023.java
@@ -0,0 +1,20 @@
+//算术运算符
+public class Test023 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("b % a = " + (b % a)); //取余 - 左操作数除以右操作数的余数
+        System.out.println("c % a = " + (c % a));
+        System.out.println("a++   = " + (a++));
+        System.out.println("a--   = " + (a--));
+        // 查看  d++ 与 ++d 的不同
+        System.out.println("d++   = " + (d++));
+        System.out.println("++d   = " + (++d));
+    }
+}
