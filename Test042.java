649d7e64bd4ee0f1f6049d4d4f7d32d1afc5d161
kakuilan <kakuilan@163.com>
2023-08-10 17:56:49 +0800

    StringBuffer 和 StringBuilder 类

---
+++ Test042.java
@@ -0,0 +1,24 @@
+//StringBuffer 和 StringBuilder 类
+public class Test042 {
+
+    public static void main(String args[]) {
+        //StringBuilder 非线程安全
+        StringBuilder sb = new StringBuilder(10);
+        sb.append("Runoob..");
+        System.out.println(sb);
+        sb.append("!");
+        System.out.println(sb);
+        sb.insert(8, "Java");
+        System.out.println(sb);
+        sb.delete(5, 8);
+        System.out.println(sb);
+
+        //StringBuffer 线程安全
+        StringBuffer sBuffer = new StringBuffer("菜鸟教程官网：");
+        sBuffer.append("www");
+        sBuffer.append(".runoob");
+        sBuffer.append(".com");
+        System.out.println(sBuffer);
+    }
+
+}
