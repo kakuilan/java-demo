614734267a712f01ec1f676b39c539d0d75edffb
kakuilan <kakuilan@163.com>
2023-08-11 18:09:28 +0800

    Matcher类 appendReplacement 和 appendTail 方法

---
+++ Test058.java
@@ -0,0 +1,21 @@
+//Matcher类 appendReplacement 和 appendTail 方法
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Test058 {
+    private static String REGEX = "a*b";
+    private static String INPUT = "aabfooaabfooabfoobkkk";
+    private static String REPLACE = "-";
+
+    public static void main(String[] args) {
+        Pattern p = Pattern.compile(REGEX);
+        // 获取 matcher 对象
+        Matcher m = p.matcher(INPUT);
+        StringBuffer sb = new StringBuffer();
+        while (m.find()) {
+            m.appendReplacement(sb, REPLACE);
+        }
+        m.appendTail(sb);
+        System.out.println(sb.toString());
+    }
+}
