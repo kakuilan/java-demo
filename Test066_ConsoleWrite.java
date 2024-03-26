9204d44e0b8a8789d860a492682629e2f5c33bb7
kakuilan <kakuilan@163.com>
2024-03-26 16:18:42 +0800

    控制台输出

---
+++ Test066_ConsoleWrite.java
@@ -0,0 +1,9 @@
+// 控制台输出
+public class Test066_ConsoleWrite {
+    public static void main(String[] args) {
+        int b;
+        b = 'A';
+        System.out.write(b);
+        System.out.write('\n');
+    }
+}
